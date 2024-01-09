24778b699fe4ff2739530cd034a4e90e194a26c2
c0d3r <strug1538@flyymail.com>
2024-01-09 02:27:11 +0000

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ DB_NAME = config("DB_NAME")
 class KeywordDeleter:
     def __init__(self, API_ID, API_HASH, BOT_TOKEN, DB_URI, DB_NAME):
         self.client = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
-        self.db = MongoClient(db_uri)[db_name]
+        self.db = MongoClient(DB_URI)[DB_NAME]
         self.keywords = set(self.db.keywords.find_one()['keywords'])
 
     async def start(self, event):
