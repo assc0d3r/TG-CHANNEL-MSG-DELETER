4c717e7780c53bd5d94f5fd141a1cd503bf03db5
c0d3r <strug1538@flyymail.com>
2024-01-09 03:36:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ logging.basicConfig(
 )
 logging.getLogger("pyrogram").setLevel(logging.DEBUG)
 
-API_ID = config("API_ID")
+API_ID = config(int("API_ID"))
 API_HASH = config("API_HASH")
 BOT_TOKEN = config("BOT_TOKEN")
 CHANNEL_NAME = config("CHANNEL_NAME")
