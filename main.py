425e73b02fab79fc296d56764d45cf8b27d6be20
c0d3r <strug1538@flyymail.com>
2024-01-09 06:22:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,8 +11,12 @@ client = TelegramClient(stringsession(SESSION), API_ID, API_HASH)
 
 async def main():
     async for message in client.iter_messages(CHANNEL_LINK):
+         blacklisted_words = item.get('blacklisted_words')
+        if blacklisted_words is not None:
         if any(word in message.text for word in blacklisted_words):
             await message.delete()
+            else:
+        print("Blacklisted_words is missing or None.")
 
 with client:
     client.loop.run_until_complete(main())
