146abb9dff0d17be6d81d0915f15831b82a7ab26
c0d3r <strug1538@flyymail.com>
2024-01-09 01:12:56 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,43 @@
-from telegram import Update, Bot
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telethon import TelegramClient, events, utils
+from pymongo import MongoClient
+import asyncio
 
 class KeywordDeleter:
-    def __init__(self, token):
-        self.updater = Updater(token=token, use_context=True)
-        self.keywords = set()
+    def __init__(self, api_id, api_hash, token, db_uri, db_name):
+        self.client = TelegramClient('bot', api_id, api_hash).start(bot_token=token)
+        self.db = MongoClient(db_uri)[db_name]
+        self.keywords = set(self.db.keywords.find_one()['keywords'])
 
-    def start(self, update: Update, context: CallbackContext):
-        context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    async def start(self, event):
+        await event.respond("I'm a bot, please talk to me!")
 
-    def add_keyword(self, update: Update, context: CallbackContext):
-        keyword = ' '.join(context.args)
+    async def add_keyword(self, event):
+        keyword = event.raw_text.split(' ', 1)[1]
         self.keywords.add(keyword)
-        context.bot.send_message(chat_id=update.effective_chat.id, text=f"Added keyword: {keyword}")
+        self.db.keywords.update_one({}, {'$set': {'keywords': list(self.keywords)}})
+        await event.respond(f"Added keyword: {keyword}")
 
-    def delete_message(self, update: Update, context: CallbackContext):
-        message_text = update.message.text
+    async def delete_message(self, event):
+        message_text = event.raw_text
         for keyword in self.keywords:
             if keyword in message_text:
-                context.bot.delete_message(chat_id=update.effective_chat.id, message_id=update.message.message_id)
+                await event.delete()
                 break
 
+    async def delete_past_messages(self, channel):
+        async for message in self.client.iter_messages(channel):
+            for keyword in self.keywords:
+                if keyword in message.text:
+                    await self.client.delete_messages(channel, message.id)
+                    break
+
     def run(self):
-        dp = self.updater.dispatcher
-        dp.add_handler(CommandHandler('start', self.start))
-        dp.add_handler(CommandHandler('addkeyword', self.add_keyword))
-        dp.add_handler(MessageHandler(Filters.text & (~Filters.command), self.delete_message))
-        self.updater.start_polling()
-        self.updater.idle()
+        self.client.add_event_handler(self.start, events.NewMessage(pattern='/start'))
+        self.client.add_event_handler(self.add_keyword, events.NewMessage(pattern='/addkeyword'))
+        self.client.add_event_handler(self.delete_message, events.NewMessage())
+        asyncio.get_event_loop().run_until_complete(self.delete_past_messages('YOUR_CHANNEL_NAME'))
+        self.client.run_until_disconnected()
 
 if __name__ == '__main__':
-    kd = KeywordDeleter('YOUR_BOT_TOKEN')
+    kd = KeywordDeleter('YOUR_API_ID', 'YOUR_API_HASH', 'YOUR_BOT_TOKEN', 'mongodb://localhost:27017', 'telegram_bot')
     kd.run()
