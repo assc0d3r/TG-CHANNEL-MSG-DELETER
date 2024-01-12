a206563618f43aad3b5d01f5bc6f7e8fcaab430b
c0d3r <strug1538@flyymail.com>
2024-01-12 21:30:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ level=logging.WARNING)
 # Define environment variables if not provided in config file
 api_id = config("API_ID")
 keywords= config("KEYWORDS")
-amogh= config("TG_SESSION_STRING")
+SESSION= config("TG_SESSION_STRING")
 channel_id= config("CHANNEL_ID")
 api_hash= config("API_HASH")
 bot_token= config("BOT_TOKEN")
 
-client = TelegramClient(StringSession(amogh), api_id, api_hash)
+client = TelegramClient(StringSession(SESSION), api_id, api_hash)
 
 async def delete_messages(keyword):
     async for message in client.iter_messages(channel_id):
